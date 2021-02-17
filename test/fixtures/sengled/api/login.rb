d6371b63598d8eaab425c10b6fa3e049d61e95fa
Grant Sparks <grantpatricksparks@gmail.com>
2021-02-17 16:06:37 -0500

    test/fixtures/sengled/api/login_valid => login_good_01 - allows us to use _good_0x and _bad_0x

--- test/fixtures/sengled/api/login.rb
+++ test/fixtures/sengled/api/login.rb
@@ -6,38 +6,38 @@ require 'net/http'
 module Sengled::Fixtures
   module API
     module Login
-      def get_login_valid_body()
-        return File.read( File.join(__dir__, "login_valid_body.json") )
-      end # get_login_valid_body()
+      def get_login_good_01_body()
+        return File.read( File.join(__dir__, "login_good_01_body.json") )
+      end # get_login_good_01_body()
 
-      def get_login_valid_header()
-        return JSON.parse( File.read( File.join(__dir__, "login_valid_header.json") ) )
-      end # get_login_valid_body()
+      def get_login_good_01_header()
+        return JSON.parse( File.read( File.join(__dir__, "login_good_01_header.json") ) )
+      end # get_login_good_01_body()
 
       # step 1) create a bogus HTTP connection for the API
       # step 2) stub Net::HTTP#request to a do-nothing dummy
       # step 3) return a bogus HTTPResponse that looks like #<HTTPOK>
-      def http_start_stub_valid(host, port, o)
+      def http_start_stub_good_01(host, port, o)
         http = Net::HTTP.new('localhost')
         http.stub(:request, nil) do
           yield(http)
         end # http.stub(:request)
         response = Net::HTTPResponse.new(1.0, 200, "OK")
-        response.body = get_login_valid_body()
-        response.instance_variable_set(:@header, get_login_valid_header())
+        response.body = get_login_good_01_body()
+        response.instance_variable_set(:@header, get_login_good_01_header())
         return response
-      end # http_start_stub_valid()
+      end # http_start_stub_good_01()
 
       # this does a few things, including preventing unwanted network traffic.
       # that Net::HTTP stub is a hassle to write every time I want this stub,
       # which is basically for every API test. since I don't want to spam their
       # server, we mask Net::HTTP.start and replace it with a stub that
       # generates a fake HTTPResponse and turns Net::HTTP#request into a dummy
-      def stub_http_start_with_valid_response()
-        Net::HTTP.stub(:start, method(:http_start_stub_valid)) do
+      def stub_http_start_with_good_01_response()
+        Net::HTTP.stub(:start, method(:http_start_stub_good_01)) do
           yield()
         end # Net::HTTP.stub(:start)
-      end # stub_http_start_with_valid_response()
+      end # stub_http_start_with_good_01_response()
     end # module Login
   end # module API
 end # module Sengled::Fixtures
--- test/sengled/api_test.rb
+++ test/sengled/api_test.rb
@@ -10,39 +10,39 @@ module Sengled::Test
     include Sengled::Fixtures::API::Login
 
     def test_new_should_start_a_session()
-      stub_http_start_with_valid_response() do
+      stub_http_start_with_good_01_response() do
         interface = Sengled::API.new(username: 'oingo', password: 'boingo')
         refute_nil(interface.instance_variable_get(:@session))
-      end # stub_http_start_with_valid_response()
+      end # stub_http_start_with_good_01_response()
     end
 
     def test_must_include_session_or_username_and_password_or_skip_login()
-      stub_http_start_with_valid_response() do
+      stub_http_start_with_good_01_response() do
         assert_raises(Sengled::API::APIError) { Sengled::API.new() }
         assert_raises(Sengled::API::APIError) { Sengled::API.new(username: 'oingo') }
         assert_raises(Sengled::API::APIError) { Sengled::API.new(password: 'boingo') }
         assert( Sengled::API.new(username: 'oingo', password: 'boingo') )
         assert( Sengled::API.new(session: 'DUMMY_SESSION') )
         assert( Sengled::API.new(skip_login: true) )
-      end # stub_http_start_with_valid_response()
+      end # stub_http_start_with_good_01_response()
     end
 
     def test_login_should_change_inspect()
-      stub_http_start_with_valid_response() do
+      stub_http_start_with_good_01_response() do
         interface = Sengled::API.new(skip_login: true)
         assert_match(/@session:INACTIVE/, interface.inspect())
         interface.login(username: 'oingo', password: 'boingo')
         assert_match(/@session:ACTIVE/, interface.inspect())
-      end # stub_http_start_with_valid_response()
+      end # stub_http_start_with_good_01_response()
     end
 
     def test_post_should_set_cookie()
-      stub_http_start_with_valid_response() do
+      stub_http_start_with_good_01_response() do
         interface = Sengled::API.new(skip_login: true)
         assert_nil(interface.instance_variable_get(:@session))
         interface.login(username: 'oingo', password: 'boingo')
         refute_nil(interface.instance_variable_get(:@session))
-      end # stub_http_start_with_valid_response()
+      end # stub_http_start_with_good_01_response()
     end
   end # class API
 end # module Sengled::Test
